d7a45ae82db0598fa68ccff798a6dd2eaa22acf2
Altemush Bhatti <altemush@gmail.com>
2021-03-19 22:45:58 -0700

    Added Components and React Router for Navigation

--- reactive-marquee/src/App.js
+++ reactive-marquee/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+ 
+import Services from './components/Services';
+import About from './components/About';
+import Industries from './components/Industries';
+import Error from './components/Error';
+import Layout from './components/Layout';
+ 
+class App extends Component {
+  render() {
+    return (      
+       <BrowserRouter>
+        <div>
+          <Layout />
+            <Switch>
+             <Route path="/" component={Services} exact/>
+             <Route path="/industries" component={Industries}/>
+             <Route path="/about" component={About}/>
+             <Route component={Error}/>
+           </Switch>
+        </div> 
+      </BrowserRouter>
+    );
+  }
 }
-
+ 
 export default App;
---
+++ reactive-marquee/src/components/Services.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Services = () => (<div></div>)
+
+export default Services;

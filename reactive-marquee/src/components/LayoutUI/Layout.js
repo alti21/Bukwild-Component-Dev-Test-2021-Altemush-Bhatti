589ec7ff447aaf74c11305410c75e037c23c518b
Altemush Bhatti <altemush@gmail.com>
2021-03-26 22:55:30 -0700

    Added Contact Us Link

--- reactive-marquee/src/components/LayoutUI/Layout.js
+++ reactive-marquee/src/components/LayoutUI/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Menu from './Menu';
 import logo from '../../logo/abc.svg';
+import Link from '../Misc/Link';
 
 const Layout = ({data, style, handleClick}) => (
         <div className="layout" style={style}>
             <img src={logo} className="App-logo" alt="logo"/>
+            <Link />
             <Menu data={data} handleClick={handleClick} />
             
         </div>
---
+++ reactive-marquee/src/components/Misc/Link.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Link = () => {
+    let style = "btn";
+    let text = "Contact Us"
+    return (
+       
+    
+        <a href="/#" className="navlinks__link navlinks__link--contact">{text}</a>
+    )
+
+}
+
+
+
+export default Link

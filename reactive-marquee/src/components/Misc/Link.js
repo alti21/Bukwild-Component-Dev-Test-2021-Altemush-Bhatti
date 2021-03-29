e25631003960a07e3c5e3ccb60e370202e1cf906
Altemush Bhatti <altemush@gmail.com>
2021-03-28 21:31:16 -0700

    Added max width to layout and marquee components

--- reactive-marquee/src/components/Misc/Link.js
+++ reactive-marquee/src/components/Misc/Link.js
@@ -1,16 +1,7 @@
 import React from 'react';
 
-const Link = ({text, className, icon}) => {
-   // let style = "btn";
-    
-    return (
-       
-    
-        <a href="/#" className={className}>{text}<i>{icon}</i></a>
-    )
-
-}
-
-
+const Link = ({text, className, icon}) => (
+    <a href="/#" className={className}>{text}<i>{icon}</i></a>
+)
 
 export default Link
